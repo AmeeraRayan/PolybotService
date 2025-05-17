f390a41edbe95b0352bead2b4374ab68b5a3533b
EC2 Default User <ec2-user@ip-10-0-0-239.eu-north-1.compute.internal>
2025-05-17 20:18:49 +0000

    DEploy final version from EC2

--- polybot/bot.py
+++ polybot/bot.py
@@ -149,7 +149,9 @@ class ImageProcessingBot(Bot):
                             if not success:
                                 self.send_text(msg['chat']['id'],"! Failed to upload image to S3.")
                                 return
-                            response = requests.post("http://localhost:8080/predict", json = {"image_name": file_name, "bucket_name":bucket_name , "region_name" :region_name})
+                            print("Sending to Yolo",file_name, bucket_name,region_name)
+                            yolo_url=os.environ["YOLO_URL"]
+                            response = requests.post(yolo_url, json = {"image_name": file_name, "bucket_name":bucket_name , "region_name" :region_name})
                             response.raise_for_status()
                             data = response.json()
                             labels = data.get("labels", [])
